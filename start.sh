39bbfa58e3e0e910bba4bf84da8d056f5f0e1d47
ketankhandelwal <ketankhandelwal70@gmail.com>
2026-04-17 16:01:35 +0530

    Commit

--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec gunicorn --workers 3 --timeout 12000 -b :5002 -e ENV=$configuration wsgi:app
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-# unlimited-astro-open — Copyright (C) 2026 Bhaktam Technologies
-# Licensed under the GNU Affero General Public License v3.0 or later.
-# See LICENSE and NOTICE in the project root for full terms.
-
-from astro_open_processor import app
-
-if __name__ == '__main__':
-    app.run()

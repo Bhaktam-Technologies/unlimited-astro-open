d7dbd9431577cb1656ece016c01374114381a16f
ketankhandelwal <ketankhandelwal70@gmail.com>
2026-04-20 20:59:03 +0530

    Add Script

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 ## Pull latest code
-#git pull https://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@github.com/Bhaktam-Technologies/unlimited-astro-open.git main
 #
 ## Build Docker image
 #sudo docker build --build-arg configuration=development -t unlimited-astro-open .
